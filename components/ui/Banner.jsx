7be955e5026675ab9ce6e0ad1176a7e7c57a6514
jumairtw <jumair.jaleel@timesworld.com>
2023-12-19 17:21:01 +0530

    banner update

--- components/ui/Banner.jsx
+++ components/ui/Banner.jsx
@@ -6,7 +6,7 @@ Banner() {
   return (
     <div className=' px-12 h-[24rem] flex mt-5'>
         <div className='bg-[#fdd2ae] w-full rounded-3xl flex relative overflow-hidden'>
-        <div className="bg-[#8d1e23] h-full flex flex-col  justify-center banner_border_radius h-[26rem] relative bottom-[1rem]">
+        <div className="bg-[#8d1e23] flex flex-col  justify-center banner_border_radius h-[26rem] relative bottom-[1rem]">
             <div className=' w-[28rem] ms-48 '  data-aos="zoom-in" data-aos-duration="100">  
                 <h3 className='text-4xl text-[#ffbd92] font-bold '>GET 10% CASHBACK ON SHOPPING $150</h3>
                 <h6 className='text-[#cc837f] my-4'>Shopping is a bit of a relaxing hobby for me, which is sometimes troubling for the bank balance.</h6>
